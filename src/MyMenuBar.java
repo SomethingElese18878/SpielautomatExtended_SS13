88e75bb2e19ca9e072b4515fb09297c6f0ff96c2
norman <norman.walker87@gmail.com>
2013-07-01 14:36:32 +0200

    refactored and extended menuItems

--- src/MyMenuBar.java
+++ src/MyMenuBar.java
@@ -5,23 +5,47 @@ import javax.swing.JMenuItem;
 
 public class MyMenuBar extends JMenuBar {
 	
-	JMenu menuProgram, menuExtensions;
-	JMenuItem itemStatus, itemExit, itemInfoWindow;
+	JMenu menuProgram;
+	JMenuItem menuProgramStatus;
+	JMenuItem menuProgramExit;
+	
+	JMenu menuExtensions;
+	JMenuItem menuExtensionsInfo;
+	JMenuItem menuExtensionsLookAndFeel;
+	JMenuItem menuExtensionsLookAndFeelLook1;
+	JMenuItem menuExtensionsLookAndFeelLook2;
+	
+	MyMenuBarListener myMenuBarListener = new MyMenuBarListener();
 	
 	public MyMenuBar() {
 		//Create menu elements
 		this.menuProgram = new JMenu("Program");
-		this.itemExit = new JMenuItem("Exit");
-		this.itemStatus = new JMenuItem("Status");
+		this.menuProgram.setMnemonic('p');
 		
-		this.menuProgram.add(this.itemStatus);
-		this.menuProgram.add(this.itemExit);
+		this.menuProgramExit = new JMenuItem("Exit");
+		this.menuProgramExit.setMnemonic('e');
+		this.menuProgramExit.addActionListener(this.myMenuBarListener);
+		this.menuProgramStatus = new JMenuItem("Status");
+		this.menuProgramStatus.addActionListener(this.myMenuBarListener);
+		
+		//add items to first menu element
+		this.menuProgram.add(this.menuProgramStatus);
+		this.menuProgram.add(this.menuProgramExit);
 		
 		//Second MenuPoint
 		this.menuExtensions = new JMenu("Extensions");
-		this.itemInfoWindow = new JMenuItem("Info");
 		
-		this.menuExtensions.add(itemInfoWindow);
+		this.menuExtensionsInfo = new JMenuItem("Info");
+		this.menuExtensionsInfo.addActionListener(this.myMenuBarListener);
+		this.menuExtensionsLookAndFeel = new JMenuItem("LookAndFeel");
+		this.menuExtensionsLookAndFeel.addActionListener(this.myMenuBarListener);
+		
+		//subMenu LookAndFeel in Extensions
+		this.menuExtensionsLookAndFeelLook1 = new JMenuItem("look1");
+		this.menuExtensionsLookAndFeel.add(menuExtensionsLookAndFeelLook1);
+		
+		this.menuExtensions.add(menuExtensionsInfo);
+		this.menuExtensions.add(menuExtensionsLookAndFeel);
 		
 		//Add menupoints to JMenuBar
 		this.add(this.menuProgram);
