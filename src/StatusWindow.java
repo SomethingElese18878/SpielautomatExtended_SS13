bddff9b28d42fdb992da2362f939b01e4e75e135
norman <norman.walker87@gmail.com>
2013-07-02 17:27:41 +0200

    Redesign statusWindow

--- src/StatusWindow.java
+++ src/StatusWindow.java
@@ -2,20 +2,27 @@ import java.awt.BorderLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.JWindow;
 
 
 public class StatusWindow extends JWindow {
 	
 	boolean visibility = true;
+	JTextField roundCount;
 
 	public StatusWindow(JFrame haupt) {
 		super(haupt);
 		System.out.println(haupt.getLocation().x + " " + haupt.getWidth() + " " + haupt.getLocation().y + " " + haupt.getHeight() + " END");
-		this.setLocation(haupt.getLocation().x + haupt.getWidth(), haupt.getLocation().y + haupt.getHeight()/2);
+		this.setLocation(haupt.getLocation().x + haupt.getWidth(), haupt.getLocation().y );
 		
-		this.add(new JButton("klar"), BorderLayout.NORTH);
-		this.add(new JButton("klarooooo"), BorderLayout.SOUTH);
+		this.roundCount = new JTextField();
+		this.roundCount.setEditable(false);
+		
+		this.add(new JLabel("Spielrunden"), BorderLayout.NORTH);
+		this.add(this.roundCount, BorderLayout.CENTER);
+		this.add(new JButton("auto"), BorderLayout.SOUTH);
 		
 		this.pack();
 		this.setVisible(this.visibility);
